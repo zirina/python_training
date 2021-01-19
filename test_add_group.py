51246d78167f41f948925cd6c82419ac8ab3e7ed
IrinaZ <irina.zhelokhovtseva@gmail.com>
2021-01-19 14:01:45 -0800

    run pytest

--- test_add_group.py
+++ test_add_group.py
@@ -1,17 +1,12 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
-
         self.wd.implicitly_wait(30)
 
     
